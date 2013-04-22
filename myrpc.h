713f60c254cf5e8a0d0a7988e45204a581f73c11
Andy <andrew@mauragis.com>
2013-04-22 14:36:56 -0400

    Added sample code for receiving arbitrary TCP messages using existing chat client to test server setup.

--- myrpc.h
+++ myrpc.h
@@ -1,7 +1,14 @@
 #ifndef MYRPC_H
 #define MYRPC_H
 
+#define OPEN_CALL   1
+#define CLOSE_CALL  2
+#define READ_CALL   3
+#define WRITE_CALL  4
+#define SEEK_CALL   5
+
 typedef struct sockaddr_in sockaddr_in_t;
+typedef struct sockaddr sockaddr_t;
 
 
 
--- rserver.c
+++ rserver.c
@@ -17,9 +17,60 @@ int main()
     int connection;
     int length;
 
-    sockaddr_in_t ourSocket;
+    sockaddr_in_t lisSocket;
+    sockaddr_in_t conSocket;
 
     // open a tcp socket
     listener = socket(AF_INET, SOCK_STREAM, 0);
+
+    // set up our socket struct to bind to
+    lisSocket.sin_family = (short) AF_INET;
+    lisSocket.sin_addr.s_addr = htonl(INADDR_ANY); // Accept any internet address
+    lisSocket.sin_port = htons(0); // Bind to any open port
+
+    // now we bind
+    // todo: error checking
+    int bindval;
+    length = sizeof(lisSocket);
+    bindval = bind(listener, (sockaddr_t*) &lisSocket, length);
+
+    // print out the port number of the socket
+    getsockname(listener, (sockaddr_t*) &lisSocket, &length);
+    printf("Server bound to port %d\n", ntohs(lisSocket.sin_port));
+
+    // begin listening, we only accept one connection in the listening queue
+    listen(listener,1);
+    length = sizeof(conSocket);
+
+    // now we accept a new connection
+    connection = accept(listener, (sockaddr_t*) &conSocket, &length);
+
+    while (1) 
+    {
+        // set up select
+        fd_set readfds, writefds;
+        struct timeval tv;
+        tv.tv_sec=0;
+        tv.tv_usec=0;
+        char ch;
+        FD_ZERO(&readfds);
+        // FD_ZERO(&writefds);
+        FD_SET(connection, &readfds);
+        // FD_SET(conn, &writefds);
+
+        int selectval = select(0, &readfds, NULL, NULL, &tv);
+        // puts("through select");
+        if (selectval == -1)
+            perror("Something bad happened with select()");
+        else
+        {
+            if (FD_ISSET(connection, &readfds))
+            {
+               
+                while(read(connection, &ch, 1) != 0)
+                    putchar(ch);   
+            }
+        }
+    }
 
 }
